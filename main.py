3e9f436035f1e81c0a871ce2914949cf5b9b6835
nicolaspal21 <nicolas.pal@mail.ru>
2022-02-20 08:11:46 +0300

    1 итерация
    Кроме переноса callback, пока не знаю как это сделать, да и осталось их всего 2 шт в конце (оставил только свой код, т.к. коллеги делают это задание самостоятельно)

--- main.py
+++ main.py
@@ -5,156 +5,184 @@ import quandl
 import matplotlib.pyplot as plt
 import math as m
 
+import constants
+
 import requests
 import datetime
 
-with open("token", "r") as f:
-    TOKEN = f.read
+# реализация через файлик
+#with open("token", "r") as f:
+#    TOKEN = f.read
 
-# подключаем базу данных
-conn = sqlite3.connect('planner_hse.db')
+# через переменные окружения
+import os
+TOKEN = os.getenv("token_bot")
 
-# курсор для работы с таблицами
-cursor = conn.cursor()
+# новая реализация, обернем в класс
+# на самом деле б/д нигде не использовалась, в процессе панического написания кода перед деделайном просто забыли удалить
 
-try:
-    # sql запрос для создания таблицы
-    query = "CREATE TABLE \"planner\" (\"ID\" INTEGER UNIQUE, \"user_id\" INTEGER, \"plan\" TEXT, PRIMARY KEY (\"ID\"))"
-    # исполняем его –> ура, теперь у нас есть таблица, куда будем все сохранять!
-    cursor.execute(query)
-except:
-    pass
+class DataBase:
+    def init_db():
+        # подключаем базу данных
+        conn = sqlite3.connect('planner_hse.db')
+
+        # курсор для работы с таблицами
+        cursor = conn.cursor()
+
+        try:
+            # sql запрос для создания таблицы
+            query = "CREATE TABLE \"planner\" (\"ID\" INTEGER UNIQUE, \"user_id\" INTEGER, \"plan\" TEXT, PRIMARY KEY (\"ID\"))"
+            # исполняем его –> ура, теперь у нас есть таблица, куда будем все сохранять!
+            cursor.execute(query)
+        except:
+            pass
+
+# вызовем метод для создания б/д
+DataBase.init_db()
+
 # подключим токен нашего бота
 bot = telebot.TeleBot(TOKEN)
 
-covid19 = COVID19Py.COVID19()
-latest = covid19.getLatest()
 
-# напишем, что делать нашему боту при команде старт
-@bot.message_handler(commands=['start'])
-def send_keyboard(message, text="Привет, чем я могу тебе помочь?"):
-    keyboard = types.ReplyKeyboardMarkup(row_width=2)  # наша клавиатура
+if __name__ == '__main__':
 
-    itembtn1 = types.KeyboardButton('Погода в Москве')
-    itembtn2 = types.KeyboardButton('Фонд Рынок')
+    # напишем, что делать нашему боту при команде старт
+    @bot.message_handler(commands=['start'])
+    def send_keyboard(message, text=constants.hello):
+        keyboard = types.ReplyKeyboardMarkup(row_width=constants.width_row)  # наша клавиатура
 
-    itembtn11 = types.KeyboardButton('Пока все!')
+        itembtn_weather_in_moscow = types.KeyboardButton(constants.weather_in_moscow)
+        itembtn_fond_market = types.KeyboardButton(constants.fond_market)
 
-    keyboard.add(itembtn1, itembtn2) # Мои
+        itembtn_fin = types.KeyboardButton(constants.fin)
 
-    keyboard.add(itembtn11)
-    # но если кнопок слишком много, они пойдут на след ряд автоматически
+        keyboard.add(itembtn_weather_in_moscow, itembtn_fond_market)
+        keyboard.add(itembtn_fin)
 
-    # пришлем это все сообщением и запишем выбранный вариант
-    msg = bot.send_message(message.from_user.id,
-                     text=text, reply_markup=keyboard)
-
-    # отправим этот вариант в функцию, которая его обработает
-    bot.register_next_step_handler(msg, callback_worker)
-
-def callback_worker(call):
-    final_message = ''
-
-    if call.text == "Погода в Москве":
-        msg_3 = bot.send_message(call.chat.id, 'Нажмите кнопку еще раз!')
-        bot.register_next_step_handler(msg_3, callback_worker_3)
-
-    elif call.text == "Фонд Рынок":
-        keyboard = types.ReplyKeyboardMarkup(row_width=2)  # наша клавиатура
-        itembtn1 = types.KeyboardButton('Нефть')  # создадим кнопку
-        itembtn2 = types.KeyboardButton('EURUSD')
-
-        keyboard.add(itembtn1,itembtn2)  # добавим кнопки 1 и 2 на первый ряд
-        #keyboard.add(itembtn3, itembtn4, itembtn5, itembtn6)  # добавим кнопки 3, 4, 5 на второй ряд
         # пришлем это все сообщением и запишем выбранный вариант
-        msg_4 = bot.send_message(call.chat.id,
-                               text=call.text, reply_markup=keyboard)
+        msg = bot.send_message(message.from_user.id,
+                         text=text, reply_markup=keyboard)
+
         # отправим этот вариант в функцию, которая его обработает
-        bot.register_next_step_handler(msg_4, callback_worker_4)
-
-    elif call.text == "Пока все!":
-        bot.send_message(call.chat.id, 'Хорошего дня! Когда захотите продолжнить нажмите на команду /start')
+        bot.register_next_step_handler(msg, callback_worker)
 
 
-@bot.message_handler(content_types=['text'])
-def handle_docs_audio(message):
-    send_keyboard(message, text="Я не понимаю :-( Выберите один из пунктов меню:")
+    def callback_worker(call):
+        final_message = ''
 
-"""
-Мои функций (Николай)
-"""
+        if call.text == constants.weather_in_moscow:
+            msg_3 = bot.send_message(call.chat.id, constants.click_again)
+            bot.register_next_step_handler(msg_3, callback_worker_weather)
 
-def get_weather(call):
-    """
-    Програмка прогноза погоды в москве с сайта  openweathermap
-    """
-    city = "moscow"
-    open_weather_token = '79505156a402ffd36412de72f4cbce46'
-    code_to_smile = {
-        "Clear": "Ясно \U00002600",
-        "Clouds": "Облачно \U00002601",
-        "Rain": "Дождь \U00002614",
-        "Drizzle": "Дождь \U00002614",
-        "Thunderstorm": "Гроза \U000026A1",
-        "Snow": "Снег \U0001F328",
-        "Mist": "Туман \U0001F32B"
-    }
-    r = requests.get(
-        f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
-    )
-    data = r.json()
-    city = data["name"]
-    cur_weather = data["main"]["temp"]
-    weather_description = data["weather"][0]["main"]
-    if weather_description in code_to_smile:
-        wd = code_to_smile[weather_description]
-    else:
-        wd = "Посмотри в окно, не пойму что там за погода!"
+        elif call.text == constants.fond_market:
+            keyboard = types.ReplyKeyboardMarkup(row_width=constants.width_row)  # наша клавиатура
+            itembtn_oil = types.KeyboardButton(constants.oil)  # создадим кнопку
+            itembtn_eur_usd = types.KeyboardButton(constants.eur_usd)
 
-    humidity = data["main"]["humidity"]
-    pressure = data["main"]["pressure"]
-    wind = data["wind"]["speed"]
-    sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
-    sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
-    length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
-        data["sys"]["sunrise"])
+            keyboard.add(itembtn_oil,itembtn_eur_usd)  # добавим кнопки на первый ряд
 
-    bot.send_message(call.chat.id, f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
-                                   f"Погода в городе: {city}\nТемпература: {cur_weather}C° {wd}\n"
-                                   f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
-                                   f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
-                                   f"***Хорошего дня!***"
-                     )
+            # пришлем это все сообщением и запишем выбранный вариант
+            msg_4 = bot.send_message(call.chat.id,
+                                   text=call.text, reply_markup=keyboard)
+            # отправим этот вариант в функцию, которая его обработает
+            bot.register_next_step_handler(msg_4, callback_worker_chart)
 
-# функция импорта графика с quandl
-def grafik(call):
-    import_name = {
-        "Нефть": "FRED/DCOILBRENTEU",
-        "EURUSD": "ECB/EURUSD"
-    }
-    quandl.ApiConfig.api_key = "Apnzsot_QRPgvhw6WyMb"
-    data = quandl.get(import_name[call.text], start_date="2021-01-01")
-    a = data.index
-    b = data['Value']
-    plt.plot(a, b)
-    plt.savefig(call.text + '.png')
-    plt.clf() # удаляет график для повторного использования
-    bot.send_photo(call.chat.id, photo=open(call.text + '.png', 'rb'))
+        elif call.text == constants.fin:
+            bot.send_message(call.chat.id, constants.good_day)
 
-"""
-Окончание блока функций моих (Николай)
 
-Мои Callback (Николай)
-"""
+    @bot.message_handler(content_types=['text'])
+    def handle_docs_audio(message):
+        send_keyboard(message, text=constants.dont_understand)
 
-# привязываем функции к кнопкам на клавиатуре
-def callback_worker_3(call):
-    get_weather(call)
-    send_keyboard(call, "Чем еще могу помочь?")
 
-def callback_worker_4(call):
+    def get_weather(call):
+        """
+        Програмка прогноза погоды в москве с сайта  openweathermap
+        """
+        city = "moscow"
+        open_weather_token = os.getenv("token_weather")
+        code_to_smile = {
+            "Clear": "Ясно \U00002600",
+            "Clouds": "Облачно \U00002601",
+            "Rain": "Дождь \U00002614",
+            "Drizzle": "Дождь \U00002614",
+            "Thunderstorm": "Гроза \U000026A1",
+            "Snow": "Снег \U0001F328",
+            "Mist": "Туман \U0001F32B"
+        }
+        r = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
+        )
+        data = r.json()
+        city = data["name"]
+        cur_weather = data["main"]["temp"]
+        weather_description = data["weather"][0]["main"]
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            wd = "Посмотри в окно, не пойму что там за погода!"
 
-    grafik(call)
-    send_keyboard(call, "Чем еще могу помочь?")
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+            data["sys"]["sunrise"])
 
-bot.polling(none_stop=True)
+
+        def weather_info():
+
+            return f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n" \
+                                       f"Погода в городе: {city}\nТемпература: {cur_weather}C° {wd}\n" \
+                                       f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n" \
+                                       f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\n" \
+                                       f"Продолжительность дня: {length_of_the_day}\n" \
+                                       f"***Хорошего дня!***"
+
+        bot.send_message(call.chat.id, weather_info())
+
+    # функция импорта графика с quandl
+    def import_chart(call):
+        import_name = {
+            "Нефть": "FRED/DCOILBRENTEU",
+            "EURUSD": "ECB/EURUSD"
+        }
+
+        def get_data_quandl():
+            '''
+            получение данных из quandl
+            '''
+            quandl.ApiConfig.api_key = os.getenv("token_quandl")
+            data = quandl.get(import_name[call.text], start_date=constants.date_for_chart)
+
+            return data
+
+        def get_chart(data):
+            '''
+            построение и сохранение графика
+            '''
+            a = data.index
+            b = data['Value']
+            plt.plot(a, b)
+            plt.savefig(call.text + '.png')
+            plt.clf() # удаляет график для повторного использования
+
+        data_q = get_data_quandl() # получим данные
+
+        get_chart(data_q) # построим график
+
+        # отправим график в виде сообщения
+        bot.send_photo(call.chat.id, photo=open(call.text + '.png', 'rb'))
+
+    # привязываем функции к кнопкам на клавиатуре
+    def callback_worker_weather(call):
+        get_weather(call)
+        send_keyboard(call, constants.help)
+
+    def callback_worker_chart(call):
+        import_chart(call)
+        send_keyboard(call, constants.help)
+
+    bot.polling(none_stop=True)
